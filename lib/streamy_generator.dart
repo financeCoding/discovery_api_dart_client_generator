1b37827cf1a08ff66a3f59e5871d1e839e04d6f4
Adam Singer <financeCoding@gmail.com>
2014-03-22 22:20:54 -0700

    removing code not needed.

--- lib/streamy_generator.dart
+++ lib/streamy_generator.dart
@@ -4,22 +4,18 @@ import 'dart:async';
 import 'dart:io' as io;
 import 'package:json/json.dart';
 import 'package:streamy/generator.dart';
-import 'dart:convert' as convert;
-import 'package:mustache/mustache.dart' as mus;
-
 
 /// Generates a Streamy API client library for a given [discoveryFile] in the
 /// pub package format.
 Future generateStreamyClientLibrary(
-    io.File discoveryFile,
-    io.Directory outputDir,
+    String discoveryFile,
+    String outputDir,
     {io.File addendumFile,
+    // TODO: the caller needs to check if directory exists before passing in here
     io.Directory templatesDir,
-    String fileName,
-    String libVersion: '0.0.0',
-    String localStreamyLocation}) {
-  var json = discoveryFile.readAsStringSync(encoding: convert.UTF8);
-  var discovery = new Discovery.fromJsonString(json);
+    String fileName
+    }) {
+  var discovery = new Discovery.fromJsonString(discoveryFile);
   var addendumData = {};
   if (addendumFile != null) {
     addendumData = parse(addendumFile.readAsStringSync());
@@ -28,7 +24,8 @@ Future generateStreamyClientLibrary(
     fileName = discovery.name;
   }
 
-  var basePath = '${outputDir.path}/$fileName';
+  // TODO: join path safely
+  var basePath = '${outputDir}/$fileName';
   var rootOut = new io.File('${basePath}.dart').openWrite();
   var resourceOut = new io.File('${basePath}_resources.dart').openWrite();
   var requestOut = new io.File('${basePath}_requests.dart').openWrite();
@@ -47,30 +44,6 @@ Future generateStreamyClientLibrary(
       objectOut,
       addendumData: addendumData,
       fileName: fileName));
-
-  var pubspecFile = new io.File('${outputDir.path}/pubspec.yaml');
-
-  var homepage = discovery.documentationLink != null
-      ? discovery.documentationLink
-      : 'https://github.com/google/streamy-dart';
-
-  var streamyVersion = '">=0.0.7"';
-  if (localStreamyLocation != null) {
-    streamyVersion = '''
-
-    path: ${localStreamyLocation}''';
-  }
-
-  mus.Template pubspecTemplate = mus.parse(templateProvider['pubspec']);
-  var pubspecData = {
-    'package_name': '${discovery.name}_${discovery.version}',
-    'version': libVersion,
-    'discovery_name': discovery.name,
-    'homepage': homepage,
-    'streamy_version': streamyVersion,
-  };
-  pubspecFile.writeAsStringSync(
-      pubspecTemplate.renderString(pubspecData, htmlEscapeValues: false));
 
   return Future.wait([
     rootOut.close(),
